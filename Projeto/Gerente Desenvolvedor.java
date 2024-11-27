59201f0f258f5688558d5ce0001f0356945567d3
Caetano174 <caetano.23104708360013@faeterj-rio.edu.br>
2024-11-27 12:48:45 -0300

    Create Gerente Desenvolvedor.java

---
+++ Projeto/Gerente Desenvolvedor.java
@@ -0,0 +1,33 @@
+package model;
+
+public class GerenteDesenvolvedor extends Funcionario implements Desenvolve, Gerencia {
+
+    public GerenteDesenvolvedor(String nome, double salario) {
+        super(nome, salario);
+    }
+
+    @Override
+    public void codar() {
+        System.out.println(getNome() + " está codando como Gerente Desenvolvedor.");
+    }
+
+    @Override
+    public void resolverProblemas() {
+        System.out.println(getNome() + " está resolvendo problemas técnicos.");
+    }
+
+    @Override
+    public void organizarEquipe() {
+        System.out.println(getNome() + " está organizando a equipe.");
+    }
+
+    @Override
+    public void conduzirReunioes() {
+        System.out.println(getNome() + " está conduzindo reuniões.");
+    }
+
+    @Override
+    public String mostrarDetalhes() {
+        return "Gerente Desenvolvedor: " + getNome() + ", Salário: " + getSalario();
+    }
+}
