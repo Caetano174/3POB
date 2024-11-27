44a8e8d3f1904e6d98f702f95a4f98401569396c
Caetano174 <caetano.23104708360013@faeterj-rio.edu.br>
2024-11-27 12:45:18 -0300

    Update Classe abstrata Funcionario.java

--- Projeto/Classe abstrata Funcionario.java
+++ Projeto/Classe abstrata Funcionario.java
@@ -3,12 +3,18 @@ package model;
 import java.io.Serializable;
 
 public abstract class Funcionario implements Serializable {
+    private int id;
     private String nome;
     private double salario;
 
-    public Funcionario(String nome, double salario) {
+    public Funcionario(int id, String nome, double salario) {
+        this.id = id;
         this.nome = nome;
         this.salario = salario;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getNome() {
@@ -25,10 +31,6 @@ public abstract class Funcionario implements Serializable {
 
     public void setSalario(double salario) {
         this.salario = salario;
-    }
-
-    public void receberAumento(double aumento) {
-        this.salario += aumento;
     }
 
     public abstract String mostrarDetalhes();
